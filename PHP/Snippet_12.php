0ce9df8e4536bcd1be33f54329d7c699d79a057b
bjmarks <bjmarks13@yahoo.com>
2023-01-12 20:23:14 -0600

    January 12, 2023 Snippet

---
+++ PHP/Snippet_12.php
@@ -0,0 +1,28 @@
+<?php
+    if(isset($_POST['submit'])) {
+        $username = $_POST["username"];
+        $password = $_POST["password"];   
+        // require_once "db_login.php";
+        echo $username . " and " . $password;
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snippet 12</title>
+    <link rel="stylesheet" href="../CSS/Snippet_13.css">
+</head>
+<body>
+    <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
+        <label for="username">Username</label>
+        <input type="text" name="username" placeholder="USERNAME">
+        <label for="password">Password</label>
+        <input type="password" name="password" placeholder="PASSWORD">
+        <button name="signup" class="btn gray">Sign Up</button>
+        <button type="submit" class="btn white" name="submit">Log In</button>
+    </form>
+</body>
+</html>
