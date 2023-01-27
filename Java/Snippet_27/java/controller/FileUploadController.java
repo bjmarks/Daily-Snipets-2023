c34ef35148d06b7d645b23041cea2259bfea1ed4
bjmarks13 <bjmarks13@yahoo.com>
2023-01-27 09:34:01 -0600

    January 27, 2023 Snippet

---
+++ Java/Snippet_27/java/controller/FileUploadController.java
@@ -0,0 +1,34 @@
+package Java.Snippet_27.java.controller;
+
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.*;
+// import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.multipart.MultipartFile;
+
+// import java.io.File;
+// import java.io.IOException;
+
+// @Controller
+public class FileUploadController {
+    // @GetMapping("/index")
+    // public String upload(){
+        // return "uploader";
+    // }
+    // @GetMapping("/return")
+    // public String redirect(){
+        // return "return";
+    // }
+    // @PostMapping("/upload")
+    // public ResponseEntity<?> fileUpload(@RequestParam("file")MultipartFile file) {
+        // String fileName = file.getOriginalFilename();
+        // fileName = "img01.jpg";
+        // try {
+            // file.transferTo(new File("C:/Users/Marks/Documents/mac/Github/UploadDemo/src/main/resources/templates/data/" + fileName));
+        // } catch (IOException e) {
+            // return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        // }
+        // return  ResponseEntity.ok("File uploaded Successfully.");
+    // }
+}
