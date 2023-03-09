3ef76e9e60ab4480cce11f90416a531e57461b77
Bjmarks <bjmarks13@yahoo.com>
2023-03-08 22:30:19 -0600

    March 08, 2023 Snippet

---
+++ Python/Snippet_67/Snippet_67.py
@@ -0,0 +1,10 @@
+number = 100
+for n in range(number):
+    if n % 3 == 0 and n % 5 == 0:
+        print("fizzbuzz")
+    elif n % 3 == 0:
+        print("fizz")
+    elif n % 5 == 0:
+        print("buzz")
+    else:
+        print(n)
