f40fc6c4362c57d2d89d5d731a618a8002676e1f
Bjmarks <bjmarks13@yahoo.com>
2023-03-15 18:53:13 -0500

    March 15, 2023 Snippet

---
+++ Swift/Snippet_74.swift
@@ -0,0 +1,17 @@
+func fizzbuzz() {
+    for i in 1...100 {
+        if i % 3 == 0 && i % 5 == 0 {
+            print("fizzbuzz")
+        }
+        else if i % 3 == 0 {
+            print("fizz")
+        }
+        else if i % 5 == 0 {
+            print("buzz")
+        }
+        else {
+            print(i)
+        }
+    }
+}
+fizzbuzz()
