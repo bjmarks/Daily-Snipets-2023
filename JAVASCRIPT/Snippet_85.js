927e6ea16e7c90dedd68ce4cad5bde1c7eda0b6c
Bjmarks <bjmarks13@yahoo.com>
2023-03-26 15:15:58 -0500

    March 26, 2023 Snippet

---
+++ JAVASCRIPT/Snippet_85.js
@@ -0,0 +1,4 @@
+import { userName, firstName, lastName } from "./Snippet_84.js";
+let app = document.getElementById("app");
+let html = firstName + " " + lastName + "<br> Is testing import and export as the username:<br>" + userName;
+app.innerHTML = html;
