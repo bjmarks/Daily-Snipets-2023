9924b34bc9bf54dc00e56f294bca9c2116af9f4a
Bjmarks <bjmarks13@yahoo.com>
2023-04-04 19:59:54 -0500

    April 04, 2023 Snippet

---
+++ JAVASCRIPT/Snippet_94.js
@@ -0,0 +1,8 @@
+import Car from "./Snippet_93.js";
+let myCar = new Car();
+myCar.make = "Hyundai";
+myCar.model = "Elantra";
+myCar.year = "2018";
+myCar.color = "lakesideBlue";
+console.log(myCar.getMake());
+myCar.driveForward();
