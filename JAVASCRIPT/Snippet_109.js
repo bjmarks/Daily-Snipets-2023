f56e7753903c2f812069b36c4b8e37709329badb
Bjmarks <bjmarks13@yahoo.com>
2023-04-19 16:04:40 -0500

    April 19, 2023 Snippet

---
+++ JAVASCRIPT/Snippet_109.js
@@ -0,0 +1,13 @@
+const USER_MAP = {
+    1: {name: "kyle"},
+    2: {name: "sally"},
+}
+const userMap = new Map([
+    [1,{name: "kyle"}],
+    [2,{name: "sally"}],
+])
+console.log(USER_MAP)
+console.log(/n/n/n/n)
+console.log(userMap)
+// userMap.set
+// USER_MAP[1]
