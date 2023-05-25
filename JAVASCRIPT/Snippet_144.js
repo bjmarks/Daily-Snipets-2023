0762b380706c473ac9a83742116e082957a47a22
Bjmarks <bjmarks13@yahoo.com>
2023-05-24 22:14:00 -0500

    May 24, 2023 Snippet

---
+++ JAVASCRIPT/Snippet_144.js
@@ -0,0 +1,14 @@
+const items = [
+    {name: "Bike", price: 100},
+    {name: "TV", price: 250},
+    {name: "Album", price: 15},
+    {name: "Book", price: 10},
+    {name: "Phone", price: 1000},
+    {name: "Mouse", price: 45},
+    {name: "Computer", price: 1200},
+    {name: "Keyboard", price: 125}
+]
+const foundItem = items.find((item) => {
+    return item.name === 'Book'
+})
+console.log(foundItem)
