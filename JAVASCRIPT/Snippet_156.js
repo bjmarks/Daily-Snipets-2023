6c233005dc87a345a250e2d69175aeafebe04cd8
Bjmarks <bjmarks13@yahoo.com>
2023-06-05 22:21:12 -0500

    June 05, 2023 Snippet

---
+++ JAVASCRIPT/Snippet_156.js
@@ -0,0 +1,5 @@
+const people = ["sally", "kyle", "john"];
+const sortedPeople = people.toSorted();
+console.log("Sorted: ", sortedPeople);
+// now a new array from people
+console.log("original: ", people);
