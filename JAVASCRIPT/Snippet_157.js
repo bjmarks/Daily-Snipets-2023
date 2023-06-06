c5b81c87fa97056dfcabd62cc87fcfb4e42aa846
Bjmarks <bjmarks13@yahoo.com>
2023-06-06 16:11:30 -0500

    June 06, 2023 Snippet

---
+++ JAVASCRIPT/Snippet_157.js
@@ -0,0 +1,5 @@
+const people = ["sally", "kyle", "john"];
+const reversedPeople = people.toReversed();
+console.log("reversed: ", reversedPeople);
+// now array from people is unaltered
+console.log("original: ", people);
