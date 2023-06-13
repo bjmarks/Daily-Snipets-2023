fb8de80b9fd55f3db238701f7b2f7cd2e6708ce4
Bjmarks <bjmarks13@yahoo.com>
2023-06-12 23:22:38 -0500

    June 12, 2023 Snippet

---
+++ C#/Snippet_163.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Snippets
+{
+  class Snippet_163
+  {
+    static void Main(string[] args)
+    {
+      string name = "Ben";
+      Console.WriteLine("Hello " + name);    
+    }
+  }
+}
