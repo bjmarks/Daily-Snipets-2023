fd1528bcef48b1ded6e78d239c4e270e12f96be3
Bjmarks <bjmarks13@yahoo.com>
2023-07-05 22:51:42 -0500

    July 05, 2023 Snippet

---
+++ TYPESCRIPT/Snippet_186.ts
@@ -0,0 +1,8 @@
+type mappedTypes =  { //mapped types in TypeScript
+    [key: string]: boolean | number;
+}
+let conforms: mappedTypes = {
+    fishes: true,
+    horses: true,
+    numberOfCats: 5
+}
