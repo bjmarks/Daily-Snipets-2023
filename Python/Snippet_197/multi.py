2ad6883cc1a79c927cd7ede95facb01482c4b395
Bjmarks <bjmarks13@yahoo.com>
2023-07-16 22:50:42 -0500

    July 16, 2023 Snippet

---
+++ Python/Snippet_197/multi.py
@@ -0,0 +1,12 @@
+a = """
+this is going to be a multi
+line string
+this 
+is
+amazing
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+Cupiditate eum corporis, totam harum architecto fugit 
+odio nulla illum voluptate tempora aspernatur deleniti expedita, 
+saepe quae enim tempore sed blanditiis ullam!
+"""
+print(a)
