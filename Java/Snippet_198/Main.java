9c25f295c2ef4841bda7709f4da22f61c5ac38bf
Bjmarks <bjmarks13@yahoo.com>
2023-07-17 21:27:23 -0500

    July 17, 2023 Snippet

---
+++ Java/Snippet_198/Main.java
@@ -0,0 +1,7 @@
+package Java.Snippet_198;
+
+public class Main {
+    int time = 20;
+    String result = (time < 18) ? "Good day." : "Good envening."; // ternary operators
+    System.out.println(result);
+}
