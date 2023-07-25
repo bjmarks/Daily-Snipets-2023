00bdebfdc185893e6533bc0003ecc966e247b977
Bjmarks <bjmarks13@yahoo.com>
2023-07-24 20:46:58 -0500

    July 24, 2023 Snippet

---
+++ Java/Snippet_205/bcrypt.java
@@ -0,0 +1,16 @@
+package Java.Snippet_205;
+
+import org.springframework.security.crypto.bcrypt.BCrypt;
+
+public class bcrypt {
+    private String password = "password";
+    private String hashed = BCrypt.hashpw(password, BCrypt.gensalt(10));
+    public void checkPass(String password) {
+        if(BCrypt.checkpw(password, hashed)) {
+            System.out.println("tight");
+        }
+        else {
+            System.out.println("wrong");
+        }
+    }
+}
