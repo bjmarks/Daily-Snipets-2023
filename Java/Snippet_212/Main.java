4cc710489a8112801c8b1f08a1a7ca4ff79a1d50
Bjmarks <bjmarks13@yahoo.com>
2023-07-31 21:18:01 -0500

    July 31, 2023 Snippet

---
+++ Java/Snippet_212/Main.java
@@ -0,0 +1,14 @@
+package Java.Snippet_212;
+
+import java.util.LinkedList;
+
+public class Main { 
+  public static void main(String[] args) { 
+    LinkedList<String> cars = new LinkedList<String>();
+    cars.add("Audi");
+    cars.add("BMW");
+    cars.add("Hyundai");
+    cars.add("Honda");
+    System.out.println(cars);
+  } 
+}
