245f4b8e796d8030bef470e1b6922d4bfdae609b
Bjmarks <bjmarks13@yahoo.com>
2023-08-02 22:34:01 -0500

    August 02, 2023 Snippet

---
+++ Java/Snippet_214/Main.java
@@ -0,0 +1,11 @@
+package Java.Snippet_214;
+
+public class Main {
+    for (int i = 0; i < 10; i++) {
+        if (i == 4) {
+          break;
+        }
+        System.out.println(i);
+      }
+         
+}
