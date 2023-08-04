d5b2795af8e58444f3153a2e6f8e5505a93228d7
Bjmarks <bjmarks13@yahoo.com>
2023-08-03 23:13:51 -0500

    August 03, 2023 Snippet

--- Java/Snippet_214/Main.java
+++ Java/Snippet_214/Main.java
@@ -1,11 +1,12 @@
 package Java.Snippet_214;
 
 public class Main {
+  public static void main(String[] args) {
     for (int i = 0; i < 10; i++) {
-        if (i == 4) {
-          break;
-        }
-        System.out.println(i);
+      if (i == 4) {
+        break;
       }
-         
+      System.out.println(i);
+    }
+  }      
 }
---
+++ Java/Snippet_215/Main.java
@@ -0,0 +1,12 @@
+package Java.Snippet_215;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            if (i == 4) {
+              continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
