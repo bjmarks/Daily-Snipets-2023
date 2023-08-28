059771d1d672d8697c7819845e1e231063f01593
Bjmarks <bjmarks13@yahoo.com>
2023-08-27 23:57:48 -0500

    August 27, 2023 Snippet

---
+++ JAVASCRIPT/Snippet_239.js
@@ -0,0 +1,8 @@
+let x = 5 & 1; // bitwise And Operator returns 1 
+
+// One bit example:         4 bits example:
+// Operation	Result      Operation	    Result
+// 0 & 0	    0           1111 & 0000	    0000
+// 0 & 1	    0           1111 & 0001	    0001
+// 1 & 0	    0           1111 & 0010	    0010
+// 1 & 1	    1           1111 & 0100	    0100
