114cd52d0e1046ba6415977c161c5be6e800b2a6
Bjmarks <bjmarks13@yahoo.com>
2023-08-28 23:28:20 -0500

    August 28, 2023 Snippet

---
+++ JAVASCRIPT/Snippet_240.js
@@ -0,0 +1,8 @@
+let x = 5 | 1; // bitwise Or Operator returns 5
+
+// One bit example:         4 bits example:
+// Operation	Result      Operation	    Result
+// 0 | 0	    0           1111 | 0000	    1111
+// 0 | 1	    1           1111 | 0001	    1111
+// 1 | 0	    1           1111 | 0010	    1111
+// 1 | 1	    1           1111 | 0100	    1111
