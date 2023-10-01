f48e7576ed65d4dc19e924800ddc9134463807aa
Bjmarks <bjmarks13@yahoo.com>
2023-09-30 20:51:36 -0500

    September 30, 2023 Snippet

---
+++ JAVASCRIPT/Snippet_273.js
@@ -0,0 +1,5 @@
+console.log(window.location);
+console.log(window.location.href);
+console.log(window.location.hostname);
+console.log(window.location.pathname);
+console.log(window.location.protocol);
